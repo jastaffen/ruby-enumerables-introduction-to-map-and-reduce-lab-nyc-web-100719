5247164dd20675be10820c3af21b2ca2484bfda3
Jack Staffen <jack.staffen@gmail.com>
2019-09-30 22:36:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,6 +5,11 @@ def map_to_negativize(array)
 end
 
 def map_to_no_change(array)
-  new_arr= array.map { |ele| }
+  new_arr = array.map { |ele| }
+  new_arr
+end
+
+def map_to_double(array)
+  new_arr = array.map { |ele| ele * 2}
   new_arr
 end
