245f1aab48b56258a4b3c0c97c77c87c48b9b5c8
Jack Staffen <jack.staffen@gmail.com>
2019-10-01 00:48:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,11 +25,5 @@ def reduce_to_total(array, starting_point = 0)
 end
 
 def reduce_to_all_true(array)
-  array.reduce(true) do |ele|
-    if ele
-      return true
-    else
-      return false
-    end
-  end      
+  array.reduce(true) {|ele|}    
 end
